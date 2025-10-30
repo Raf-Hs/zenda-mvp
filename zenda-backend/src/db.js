0b9a4b5fc5945adb032e1b3ce197750a93308460
rafael <rafael.silva.06@outlook.com>
2025-10-29 18:44:40 -0600

    Db original

--- zenda-backend/src/db.js
+++ zenda-backend/src/db.js
@@ -1,17 +1,2 @@
 import { PrismaClient } from "@prisma/client";
-
-let prisma;
-
-if (!global.prisma) {
-  global.prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL + "?connection_limit=1&pool_timeout=10",
-      },
-    },
-  });
-}
-
-prisma = global.prisma;
-
-export default prisma;
+export const prisma = new PrismaClient();
